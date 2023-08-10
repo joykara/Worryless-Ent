579f958e85f7f42183534700c48252f953b7369a
joykara <joykarani1@gmail.com>
2023-08-10 13:28:23 +0300

    update

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 //export components
 export { default as Header } from './Header';
+export { default as Navbar } from './Navbar';
 export { default as About } from './About';
 export { default as Footer } from './Footer';
 export { default as Services } from './Services';
